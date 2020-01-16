337e335b12950921cdfa1612c77840b8954063d8
AMasel <40176679+MaslaLT@users.noreply.github.com>
2020-01-16 11:35:36 +0200

    PCA-65/Add_required_routes Edittet controllers

--- app/controllers/Admin/IndexController.php
+++ app/controllers/Admin/IndexController.php
@@ -1,20 +1,16 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Controllers\Admin;
 
+use App\Controllers\Controller;
 use App\Core\Config;
 use App\Topic;
 
-class indexController extends Controller
+class IndexController extends Controller
 {
-
-    public function __construct()
-    {
-        // Home Controller
-    }
-
     public function index()
     {
+        echo ('<h1>This is admin panel</h1>');
         return view('index', ["title" => Config::get('config', 'name')]);
     }
 }
