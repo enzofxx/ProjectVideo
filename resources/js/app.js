1b92f80921e896a8b7b1201a396e42935061ae43
Azuolas <azuolas.slipavicius@gmail.com>
2020-01-23 09:52:37 +0200

    PCA-78/ Update course add/edit back

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,7 @@ import '../css/style.scss';
 import 'jquery/dist/jquery.slim.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'popper.js/dist/popper.min.js';
+
+// course/add/ reset button jquery, removes img tag
+
+$('#OpenImgUpload').click(function(){ $('#coursePicture').trigger('click'); });
