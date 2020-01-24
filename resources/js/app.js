e760cfa2028920eebc2212e8164cbb3f17aaa022
Azuolas <azuolas.slipavicius@gmail.com>
2020-01-24 13:47:31 +0200

    fixed issue with Jquery not loading added JS functions to add and edit course

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,18 @@ import 'jquery/dist/jquery.slim.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'popper.js/dist/popper.min.js';
 
-// course/add/ reset button jquery, removes img tag
+// admin/course/{id}/edit opens file input field
+$('#OpenImgUpload').click(function(){
+    $('#coursePicture').trigger('click');
+});
 
-$('#OpenImgUpload').click(function(){ $('#coursePicture').trigger('click'); });
+// admin/course/{id}/edit and admin/course/add Preview selected file
+$('#coursePicture').change(function () {
+    var url = URL.createObjectURL(this.files[0]);
+    $('#pic').attr('src', url);
+});
+
+// admin/course/{id}/edit reset button jquery, removes img tag
+$('#clearForm').click(function () {
+    $('#pic').attr('src', '');
+});
