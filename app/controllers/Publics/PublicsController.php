e071952da0949007c04d0efc7e48c8732f31a7d9
AMasel <40176679+MaslaLT@users.noreply.github.com>
2020-01-28 20:36:44 +0200

    PCA-87/ended front end part of main page

--- app/controllers/Publics/PublicsController.php
+++ app/controllers/Publics/PublicsController.php
@@ -8,7 +8,27 @@ class PublicsController extends Controller
 {
     public function index()
     {
+        $route = 'home';
         $courses = Courses::select('*')->getAll();
-        return view('public/index', ['courses' => $courses]);
+        return view('public/index', [
+            'courses' => $courses,
+            'route' => $route,
+            ]);
+    }
+
+    public function about()
+    {
+        $route = 'about';
+        return view('public/aboutUs', [
+            'route' => $route,
+        ]);
+    }
+
+    public function feedback()
+    {
+        $route = 'feedback';
+        return view('public/feedback', [
+            'route' => $route,
+        ]);
     }
 }
--- config/routes.php
+++ config/routes.php
@@ -28,30 +28,19 @@ Router::get("/admin/income", "Admin\IncomeController@index")->name('admin.income
 /*---- FrontOffice ----*/
 /* Index */
 Router::get("", "Publics\PublicsController@index")->name('home');
+
+/* About Us */
+Router::get("/about", "Publics\PublicsController@about")->name('about');
+
+/* Feedback */
+Router::get("/feedback", "Publics\PublicsController@feedback")->name('feedback');
+
+/* Courses */
 Router::get("/course/{course}", "CourseController@show")->name('course.show');
 
 
 
 /* EXAMPLES */
-
-/* Topic  */
-
-//Router::get("/topic", "PublicController@index")->name('topic.index');
-//Router::get("/topic/create", "TopicController@create")->name('topic.create');
-//Router::get("/topic/search", "TopicController@search")->name('topic.search');
-//Router::get("/topic/{topic}", "TopicController@show")->name('topic.show');
-//Router::get("/topic/{topic}/edit", "TopicController@edit")->name('topic.edit');
-//Router::patch("/topic/{topic}", "TopicController@update")->name('topic.update');
-//Router::delete("/topic/{topic}", "TopicController@delete")->name('topic.delete');
-//Router::put("/topic", "TopicController@store")->name('topic.store');
-//Router::get("/topic/{topic}/examples", "ExampleController@index")->name('example.index');
-//Router::get("/topic/{topic}/examples/create", "ExampleController@create")->name('example.create');
-//Router::get("/topic/{topic}/examples/{example}", "ExampleController@show")->name('example.show');
-//Router::get("/topic/{topic}/examples/{example}/edit", "ExampleController@edit")->name('example.edit');
-//Router::patch("/topic/{topic}/examples/{example}", "ExampleController@update")->name('example.update');
-//Router::delete("/topic/{topic}/examples/{example}", "ExampleController@delete")->name('example.delete');
-//Router::put("/topic/{topic}/examples", "ExampleController@store")->name('example.store');
-
 
 /* API */
 
